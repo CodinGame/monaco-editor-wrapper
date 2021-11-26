9058b94efe9d3d54e4bd151c4c5c0e8ffd13878e
Loïc Mangeonjean <loic@codingame.com>
2021-11-26 15:52:31 +0100

    Increase semantic token delay

---
+++ src/types/hacks.d.ts
@@ -0,0 +1,5 @@
+declare module 'monaco-editor/esm/vs/editor/common/services/modelServiceImpl' {
+  export class ModelSemanticColoring {
+    public static FETCH_DOCUMENT_SEMANTIC_TOKENS_DELAY: number
+  }
+}
