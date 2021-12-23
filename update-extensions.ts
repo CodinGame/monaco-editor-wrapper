48b3055ad942f080078cdae8baf489b711dfb024
Loïc Mangeonjean <loic@codingame.com>
2021-12-23 16:12:42 +0100

    feat: Improve extensions update script to:
    - include more extensions
    - Split language configuration in lazy loaded files
    - Store semantic token configuration
    - Store the extension configuration schema
    - Translate the package.json

--- update-extensions.ts
+++ update-extensions.ts
@@ -4,31 +4,24 @@ import ini from 'ini'
 import cson from 'cson-parser'
 import plist from 'fast-plist'
 import YAML from 'yaml'
+import type * as monaco from 'monaco-editor'
 import https from 'https'
 import path from 'path'
-import { promisify } from 'util'
-import * as fs from 'fs'
+import * as fs from 'fs/promises'
 
-const writeFile = promisify(fs.writeFile)
-const mkdir = promisify(fs.mkdir)
-
-const excludeScopeNames = ['source.tsx', 'source.js.jsx', 'source.objcpp', 'source.reason', 'source.cpp.embedded.macro']
-
-const extensionsPath = path.resolve(__dirname, 'src/languages/extensions')
-
-interface Extension {
-  name: string
-  repository: string
-  path?: string
-  version?: string
-  mapping?: Record<string, string>
-}
+const overrideConfigurationDefaultValue = new Map<string, unknown>(Object.entries({
+  'cobol-lsp.subroutine-manager.paths-local': ['/tmp/project'],
+  'Lua.runtime.version': 'Lua 5.4',
+  'Lua.diagnostics.enable': true,
+  'Lua.diagnostics.disable': ['lowercase-global'],
+  'r.lsp.diagnostics': false
+}))
 
 const extensions: Extension[] = [
   ...['clojure', 'coffeescript', 'cpp', 'csharp', 'css', 'fsharp', 'go',
     'groovy', 'html', 'java', 'javascript', 'json', 'lua', 'markdown-basics',
     'objective-c', 'perl', 'php', 'powershell', 'python', 'r', 'ruby',
-    'rust', 'scss', 'shellscript', 'swift', 'typescript-basics',
+    'rust', 'scss', 'shellscript', 'swift', 'typescript-basics', 'typescript-language-features',
     'vb', 'xml', 'yaml'].map(language => ({
     name: language,
     repository: 'microsoft/vscode',
@@ -83,8 +76,70 @@ const extensions: Extension[] = [
   }, {
     name: 'postgresql',
     repository: 'Borvik/vscode-postgres'
+  }, {
+    name: 'java',
+    repository: 'redhat-developer/vscode-java'
+  }, {
+    name: 'lua',
+    repository: 'sumneko/vscode-lua'
+  }, {
+    name: 'cpptools',
+    repository: 'microsoft/vscode-cpptools',
+    path: 'Extension/'
+  }, {
+    name: 'omnisharp',
+    repository: 'OmniSharp/omnisharp-vscode'
+  }, {
+    name: 'vscode-python',
+    repository: 'microsoft/vscode-python'
+  }, {
+    name: 'vscode-R',
+    repository: 'REditorSupport/vscode-R'
+  }, {
+    name: 'Groovy',
+    repository: 'GroovyLanguageServer/groovy-language-server',
+    path: 'vscode-extension/'
   }
 ]
+
+const excludeScopeNames = ['source.tsx', 'source.js.jsx', 'source.objcpp', 'source.reason', 'source.cpp.embedded.macro']
+
+const extensionsPath = path.resolve(__dirname, 'src/languages/extensions')
+
+function overrideDefaultValue (configuration: monaco.extra.IConfigurationNode) {
+  return {
+    ...configuration,
+    properties: Object.fromEntries(Object.entries(configuration.properties ?? {}).map(([key, value]) => [
+      key, {
+        ...value,
+        default: overrideConfigurationDefaultValue.get(key) ?? value.default
+      }
+    ]))
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function applyI18n (object: any, i18n: Partial<Record<string, string>>): any {
+  if (object === null) {
+    return object
+  } else if (typeof object === 'string') {
+    return object.replace(/^%(.*)%$/g, (g, g1) => i18n[g1] ?? g1)
+  } else if (Array.isArray(object)) {
+    return object.map(item => applyI18n(item, i18n))
+  } else if (typeof object === 'object') {
+    return Object.fromEntries(Object.entries(object).map(([key, value]) => [key, applyI18n(value, i18n)]))
+  } else {
+    return object
+  }
+}
+
+interface Extension {
+  name: string
+  repository: string
+  path?: string
+  version?: string
+  mapping?: Record<string, string>
+}
 
 function download (url: string, redirectCount?: number): Promise<string | null> {
   return new Promise((resolve, reject) => {
@@ -114,6 +169,35 @@ function download (url: string, redirectCount?: number): Promise<string | null> 
       reject(err.message)
     })
   })
+}
+
+interface ITokenTypeExtensionPoint {
+  id: string
+  description: string
+  superType?: string
+}
+
+interface ITokenModifierExtensionPoint {
+  id: string
+  description: string
+}
+interface ITokenStyleDefaultExtensionPoint {
+  language?: string
+  scopes: { [selector: string]: string[] }
+}
+
+interface PackageJsonContributes {
+  languages?: (monaco.languages.ILanguageExtensionPoint & { configuration?: string })[]
+  grammars?: monaco.extra.ITMSyntaxExtensionPoint[]
+  configurationDefaults?: Record<string, unknown>
+  snippets?: {
+    language: string
+    path: string
+  }[]
+  semanticTokenTypes?: ITokenTypeExtensionPoint[]
+  semanticTokenScopes?: ITokenStyleDefaultExtensionPoint[]
+  semanticTokenModifiers?: ITokenModifierExtensionPoint[]
+  configuration?: monaco.extra.IConfigurationNode
 }
 
 async function createRepositoryFileResolver (extension: Extension) {
@@ -153,18 +237,36 @@ async function createRepositoryFileResolver (extension: Extension) {
 }
 
 async function fetchExtensions () {
-  let grammarResult: unknown[] = []
+  await fs.rmdir(extensionsPath, { recursive: true })
+
+  let grammarResult: Omit<monaco.extra.ITMSyntaxExtensionPoint, 'path'>[] = []
   let grammarPaths: Record<string, string> = {}
   let snippetPaths: Record<string, string> = {}
-  let languageResult: unknown[] = []
+  let languageConfigurationPaths: Record<string, string> = {}
+  let languageResult: Omit<monaco.languages.ILanguageExtensionPoint, 'configuration'>[] = []
 
   const grammarsPath = path.resolve(extensionsPath, 'grammars')
-  await mkdir(grammarsPath, { recursive: true })
+  await fs.mkdir(grammarsPath, { recursive: true })
 
   const snippetsPath = path.resolve(extensionsPath, 'snippets')
-  await mkdir(snippetsPath, { recursive: true })
+  await fs.mkdir(snippetsPath, { recursive: true })
 
-  let configurationDefaultsResult = {}
+  const languageConfigurationsPath = path.resolve(extensionsPath, 'languageConfigurations')
+  await fs.mkdir(languageConfigurationsPath, { recursive: true })
+
+  const extensionResult: {
+    configurationDefaults: Record<string, unknown>
+    semanticTokenTypes: ITokenTypeExtensionPoint[]
+    semanticTokenScopes: ITokenStyleDefaultExtensionPoint[]
+    semanticTokenModifiers: ITokenModifierExtensionPoint[]
+    configurations: monaco.extra.IConfigurationNode[]
+  } = {
+    configurationDefaults: {},
+    semanticTokenTypes: [],
+    semanticTokenScopes: [],
+    semanticTokenModifiers: [],
+    configurations: []
+  }
 
   let i = 0
   for (const extension of extensions) {
@@ -172,89 +274,124 @@ async function fetchExtensions () {
     const resolve = await createRepositoryFileResolver(extension)
 
     const packageJsonContent = (await download(resolve('package.json')))!
-    const packageJson = JSON.parse(packageJsonContent)
+    let packageJson = JSON.parse(packageJsonContent) as {
+        contributes: PackageJsonContributes
+      }
 
-    const { languages, grammars, configurationDefaults, snippets } = packageJson.contributes
+    // Only use the default i18n, can be improved
+    const packageNlsJsonContent = await download(resolve('package.nls.json'))
+    if (packageNlsJsonContent != null) {
+      packageJson = applyI18n(packageJson, JSON.parse(packageNlsJsonContent))
+    }
+
+    const {
+      languages,
+      grammars,
+      configurationDefaults,
+      snippets,
+      semanticTokenTypes,
+      semanticTokenScopes,
+      semanticTokenModifiers,
+      configuration
+    } = packageJson.contributes
 
     if (configurationDefaults != null) {
-      configurationDefaultsResult = {
-        ...configurationDefaultsResult,
+      extensionResult.configurationDefaults = {
+        ...extensionResult.configurationDefaults,
         ...configurationDefaults
       }
     }
 
-    for (const grammarConf of grammars) {
-      if (excludeScopeNames.includes(grammarConf.scopeName)) {
-        continue
-      }
-      const { path: grammarRemotePath, ...grammarWithoutPath } = grammarConf
+    if (semanticTokenTypes != null) {
+      extensionResult.semanticTokenTypes.push(...semanticTokenTypes)
+    }
+    if (semanticTokenScopes != null) {
+      extensionResult.semanticTokenScopes.push(...semanticTokenScopes)
+    }
+    if (semanticTokenModifiers != null) {
+      extensionResult.semanticTokenModifiers.push(...semanticTokenModifiers)
+    }
 
-      const grammarUrl = resolve(grammarRemotePath)
-
-      const grammarText = await download(grammarUrl)
-      if (grammarText == null) {
-        throw new Error('Unable to download grammar file')
-      }
-      const ext = path.extname(grammarUrl)
-
-      let grammarObject = null
-      try {
-        if (ext === '.tmLanguage' || ext === '.plist') {
-          grammarObject = plist.parse(grammarText)
-        } else if (ext === '.cson') {
-          grammarObject = cson.parse(grammarText)
-        } else if (ext === '.json' || ext === '.JSON-tmLanguage') {
-          grammarObject = JSON5.parse(grammarText)
-        } else if (ext === '.yaml' || ext === '.yml') {
-          grammarObject = YAML.parse(grammarText)
-        } else {
-          return Promise.reject(new Error('Unknown file extension: ' + ext))
+    if (grammars != null) {
+      for (const grammarConf of grammars) {
+        if (excludeScopeNames.includes(grammarConf.scopeName)) {
+          continue
         }
-      } catch (error) {
-        console.error(error)
-        throw new Error('Invalid grammar file ' + grammarUrl)
-      }
+        const { path: grammarRemotePath, ...grammarWithoutPath } = grammarConf
+        const grammarUrl = resolve(grammarRemotePath)
 
-      const result = {
-        ...grammarObject,
-        information_for_contributors: [
-          `This file has been converted from https://github.com/${extension.repository}/blob/${extension.version || 'master'}/${extension.path ?? ''}`,
-          'If you want to provide a fix or improvement, please create a pull request against the original repository.',
-          'Once accepted there, we are happy to receive an update request.'
+        const grammarText = await download(grammarUrl)
+        if (grammarText == null) {
+          throw new Error('Unable to download grammar file')
+        }
+        const ext = path.extname(grammarUrl)
+
+        let grammarObject = null
+        try {
+          if (ext === '.tmLanguage' || ext === '.plist') {
+            grammarObject = plist.parse(grammarText)
+          } else if (ext === '.cson') {
+            grammarObject = cson.parse(grammarText)
+          } else if (ext === '.json' || ext === '.JSON-tmLanguage') {
+            grammarObject = JSON5.parse(grammarText)
+          } else if (ext === '.yaml' || ext === '.yml') {
+            grammarObject = YAML.parse(grammarText)
+          } else {
+            return Promise.reject(new Error('Unknown file extension: ' + ext))
+          }
+        } catch (error) {
+          console.error(error)
+          throw new Error('Invalid grammar file ' + grammarUrl)
+        }
+
+        const result = {
+          ...grammarObject,
+          information_for_contributors: [
+            `This file has been converted from https://github.com/${extension.repository}/blob/${extension.version ?? 'master'}/${extension.path ?? ''}`,
+            'If you want to provide a fix or improvement, please create a pull request against the original repository.',
+            'Once accepted there, we are happy to receive an update request.'
+          ]
+        }
+
+        const basename = path.basename(grammarUrl)
+        const fileName = ext === '.json' ? basename : `${basename}.json`
+        const filePath = path.join(extension.name, fileName)
+
+        await fs.mkdir(path.resolve(grammarsPath, extension.name), { recursive: true })
+        await fs.writeFile(path.resolve(grammarsPath, filePath), JSON.stringify(result, null, 2))
+
+        grammarResult = [
+          ...grammarResult,
+          grammarWithoutPath
         ]
-      }
-
-      const basename = path.basename(grammarUrl)
-      const fileName = ext === '.json' ? basename : `${basename}.json`
-      const filePath = path.join(extension.name, fileName)
-
-      await mkdir(path.resolve(grammarsPath, extension.name), { recursive: true })
-      await writeFile(path.resolve(grammarsPath, filePath), JSON.stringify(result, null, 2))
-
-      grammarResult = [
-        ...grammarResult,
-        grammarWithoutPath
-      ]
-      grammarPaths = {
-        ...grammarPaths,
-        [grammarConf.scopeName]: path.join('grammars', filePath)
+        grammarPaths = {
+          ...grammarPaths,
+          [grammarConf.scopeName]: path.join('grammars', filePath)
+        }
       }
     }
 
     if (languages != null) {
       for (const languageConf of languages) {
         const { configuration: languageConfigurationPath, ...languageWithoutConfiguration } = languageConf
+        const id = languageWithoutConfiguration.id
         let configuration = null
         if (languageConfigurationPath != null) {
           const configurationUrl = resolve(languageConfigurationPath)
           configuration = JSON5.parse((await download(configurationUrl))!)
+
+          const filePath = `${id}.json`
+          await fs.writeFile(path.resolve(languageConfigurationsPath, filePath), JSON.stringify(configuration, null, 2))
+
+          languageConfigurationPaths = {
+            ...languageConfigurationPaths,
+            [id]: path.join('languageConfigurations', filePath)
+          }
         }
 
         languageResult = [
-          ...languageResult, {
-            ...languageWithoutConfiguration,
-            configuration
-          }
+          ...languageResult,
+          languageWithoutConfiguration
         ]
       }
     }
@@ -265,22 +402,27 @@ async function fetchExtensions () {
         const snippetUrl = resolve(snippetPath)
         const snippetFileContent = JSON5.parse((await download(snippetUrl))!)
 
-        const filePath = `${language}.json`
-        await writeFile(path.resolve(snippetsPath, filePath), JSON.stringify(snippetFileContent, null, 2))
+        const filePath = `${extension.name}-${snippetConf.language}.json`
+        await fs.writeFile(path.resolve(snippetsPath, filePath), JSON.stringify(snippetFileContent, null, 2))
         snippetPaths = {
           ...snippetPaths,
           [language]: path.join('snippets', filePath)
         }
       }
     }
+
+    if (configuration != null) {
+      extensionResult.configurations.push(overrideDefaultValue(configuration))
+    }
   }
 
   return {
     grammars: grammarResult,
-    grammarPaths,
     languages: languageResult,
-    configurationDefaults: configurationDefaultsResult,
-    snippetPaths
+    extensions: extensionResult,
+    grammarPaths,
+    snippetPaths,
+    languageConfigurationPaths
   }
 }
 
@@ -296,25 +438,32 @@ function generateSnippetLoaderLine ([language, path]: [string, string]) {
   return generateLoaderLine(language, `snippet-${language}`, path)
 }
 
+function generateLanguageConfigurationLoaderLine ([language, path]: [string, string]) {
+  return generateLoaderLine(language, `configuration-${language}`, path)
+}
+
 async function main () {
   const {
     grammars,
-    grammarPaths,
     languages,
-    configurationDefaults,
-    snippetPaths
+    extensions,
+    snippetPaths,
+    grammarPaths,
+    languageConfigurationPaths
   } = await fetchExtensions()
 
-  await mkdir(path.dirname(extensionsPath), { recursive: true })
-  await writeFile(path.resolve(extensionsPath, 'languages.json'), JSON.stringify(languages, null, 2))
+  await fs.mkdir(path.dirname(extensionsPath), { recursive: true })
+  await fs.writeFile(path.resolve(extensionsPath, 'languages.json'), JSON.stringify(languages, null, 2))
 
-  await writeFile(path.resolve(extensionsPath, 'grammars.json'), JSON.stringify(grammars, null, 2))
+  await fs.writeFile(path.resolve(extensionsPath, 'grammars.json'), JSON.stringify(grammars, null, 2))
 
-  await writeFile(path.resolve(extensionsPath, 'configurationDefaults.json'), JSON.stringify(configurationDefaults, null, 2))
+  const { configurations, ...extensionsWithoutConfiguration } = extensions
+  await fs.writeFile(path.resolve(extensionsPath, 'extensions.json'), JSON.stringify(extensionsWithoutConfiguration, null, 2))
+  await fs.writeFile(path.resolve(extensionsPath, 'extensionConfigurations.json'), JSON.stringify(configurations, null, 2))
 
   const grammarLoaders = `{\n${Object.entries(grammarPaths).map(generateGrammarLoaderLine).join(',\n')}\n}\n`
 
-  await writeFile(path.resolve(extensionsPath, 'grammarLoader.ts'), `
+  await fs.writeFile(path.resolve(extensionsPath, 'grammarLoader.ts'), `
 // Generated file, do not modify
 
 /* eslint-disable */
@@ -323,11 +472,20 @@ export default ${grammarLoaders}
 
   const snippetLoaders = `{\n${Object.entries(snippetPaths).map(generateSnippetLoaderLine).join(',\n')}\n}\n`
 
-  await writeFile(path.resolve(extensionsPath, 'snippetLoader.ts'), `
+  await fs.writeFile(path.resolve(extensionsPath, 'snippetLoader.ts'), `
 // Generated file, do not modify
 
 /* eslint-disable */
 export default ${snippetLoaders}
+  `)
+
+  const configurationLoader = `{\n${Object.entries(languageConfigurationPaths).map(generateLanguageConfigurationLoaderLine).join(',\n')}\n}\n`
+
+  await fs.writeFile(path.resolve(extensionsPath, 'configurationLoader.ts'), `
+// Generated file, do not modify
+
+/* eslint-disable */
+export default ${configurationLoader}
   `)
 }
 
