caac02d007903026300bc4b7c2a351e73947b643
Loïc Mangeonjean <loic@codingame.com>
2022-08-16 15:09:19 +0200

    feat: register some colors used by most apps

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,28 @@
 import { setThemes, setDefaultThemes, IThemeExtensionPoint } from 'vscode/service-override/theme'
 import { Disposable } from 'vscode'
+import { registerColor } from 'vscode/monaco'
 import defaultThemes from '../languages/extensions/themes.json'
 import themeLoader from '../languages/extensions/themeLoader'
+
+// Export 3 new colors used
+registerColor('statusBar.foreground', {
+  dark: '#ffffff',
+  light: '#20252a',
+  hcDark: '#ffffff',
+  hcLight: '#20252a'
+}, 'Status bar foreground color.')
+registerColor('statusBar.background', {
+  dark: '#252e38',
+  light: '#ffffff',
+  hcDark: null,
+  hcLight: null
+}, 'Status bar background color.')
+registerColor('statusBar.border', {
+  dark: '#41454a',
+  light: '#dadada',
+  hcDark: '#41454a',
+  hcLight: '#dadada'
+}, 'Status bar border color.')
 
 interface DefaultTheme extends IThemeExtensionPoint {
   extension: string
