123308419ddc09c745fcd5c2e1a740a10e81e296
Loïc Mangeonjean <loic@coderpad.io>
2022-12-08 18:24:15 +0100

    fix: override tooltip heading style

---
+++ src/style.ts
@@ -0,0 +1,11 @@
+export function addStyle (styleString: string): void {
+  const style = document.createElement('style')
+  style.textContent = styleString
+  document.head.append(style)
+}
+
+addStyle(`
+.monaco-hover-content h1, .monaco-hover-content h2, .monaco-hover-content h3, .monaco-hover-content h4, .monaco-hover-content h5, .monaco-hover-content h6 {
+  all: revert;
+}
+`)
