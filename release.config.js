0d27bee118ed1cf931eb4de652cd5f3319f6c088
Loïc Mangeonjean <loic@coderpad.io>
2023-02-24 12:05:36 +0100

    ci: fix semantic release config

--- release.config.js
+++ release.config.js
@@ -1,12 +1,10 @@
 module.exports = {
+  ...require('@codingame/semantic-release-config'),
   branches: [
     'main'
   ],
   plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'conventionalcommits',
-      releaseRules: '@codingame/semantic-release-rules'
-    }],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github'
