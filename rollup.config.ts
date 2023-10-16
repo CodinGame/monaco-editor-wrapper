cdc33f10d1cea9c6bca828e69691a1ec8a5b35e8
Loïc Mangeonjean <loic@coderpad.io>
2023-10-16 11:45:18 +0200

    feat: update rollup config
    - do not force workers as internal
    - add vsix plugin
    - do not use babel anymore

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,20 +1,14 @@
-import json from 'rollup-plugin-json5'
 import { visualizer } from 'rollup-plugin-visualizer'
 import commonjs from '@rollup/plugin-commonjs'
 import alias from '@rollup/plugin-alias'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import webWorkerLoader from 'rollup-plugin-web-worker-loader'
 import { string } from 'rollup-plugin-string'
-import { babel } from '@rollup/plugin-babel'
+import typescript from '@rollup/plugin-typescript'
 import * as rollup from 'rollup'
 import builtins from 'rollup-plugin-node-builtins'
-import { terser } from 'rollup-plugin-terser'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import globImport from 'rollup-plugin-glob-import'
+import vsixPlugin from '@codingame/monaco-vscode-rollup-vsix-plugin'
 import pkg from './package.json' assert { type: 'json' }
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 const externals = Object.keys(pkg.dependencies)
 
@@ -32,38 +26,51 @@ export default rollup.defineConfig({
     if (/\.wasm$/.test(source)) {
       return true
     }
-    if ([/\?worker$/].some(reg => reg.test(source))) {
-      return false
-    }
     return externals.some(external => source === external || source.startsWith(`${external}/`))
   },
   output: [{
-    chunkFileNames: '[name].js',
     dir: 'dist',
     format: 'esm',
     paths: {
-      'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api',
-      'monaco-editor-core': 'monaco-editor/esm/vs/editor/editor.api'
+      'monaco-editor-core': 'monaco-editor'
     },
-    entryFileNames: (module) => {
-      const name = path.join(
-        path.dirname(
-          path.relative(
-            path.join(__dirname, 'src'),
-            module.facadeModuleId!
-          )
-        ),
-        module.name
-      )
-      return `${name}.js`
-    }
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+    entryFileNames: '[name].js'
   }],
   plugins: [
-    builtins() as rollup.Plugin,
-    webWorkerLoader({
-      targetPlatform: 'browser',
-      pattern: /(.+)\?worker$/,
-      plugins: [terser()]
+    builtins(),
+    globImport({
+      format: 'import'
+    }),
+    vsixPlugin({
+      rollupPlugins: [],
+      transformManifest (manifest) {
+        const {
+          commands,
+          debuggers,
+          keybindings,
+          menus,
+          views,
+          walkthroughs,
+          breakpoints,
+          taskDefinitions,
+          viewsWelcome,
+          ...remainingContribute
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } = manifest.contributes as any
+
+        const {
+          browser,
+          extensionDependencies, // for pure-d that requires hbenl.vscode-test-explorer
+          ...remainingManifest
+        } = manifest
+
+        return {
+          ...remainingManifest,
+          contributes: remainingContribute
+        }
+      }
     }),
     nodeResolve({
       extensions
@@ -71,30 +78,14 @@ export default rollup.defineConfig({
     commonjs({
       esmExternals: (id) => id.match(/^monaco-editor(\/.*)?/) != null // required for monaco-emacs with use import monaco-editor esm code from commonjs code
     }),
-    babel({
-      extensions,
-      presets: [
-        ['@babel/preset-env', {
-          modules: false
-        }],
-        '@babel/preset-typescript'
-      ],
-      plugins: [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-optional-chaining'
-      ],
-      babelHelpers: 'bundled',
-      exclude: /node_modules\//
+    typescript({
+      noEmitOnError: true
     }),
-    json(),
     visualizer(),
     alias({
       entries: [{
         find: /^monaco-editor-core\//,
         replacement: 'monaco-editor/'
-      }, {
-        find: /^(monaco-editor|monaco-editor-core)$/,
-        replacement: 'monaco-editor/esm/vs/editor/editor.api'
       }]
     }),
     string({
