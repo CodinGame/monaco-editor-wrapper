c45f20bc31a9c74420bb3e451bde2d8343b3dd05
Loïc Mangeonjean <loic@coderpad.io>
2023-10-16 11:05:22 +0200

    cleanup: remove deprecated theme management

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,4 @@
-import { setThemes, setDefaultThemes, IThemeExtensionPoint } from 'vscode/service-override/theme'
-import { Disposable } from 'vscode'
 import { registerColor } from 'vscode/monaco'
-import defaultThemes from '../languages/extensions/themes.json'
-import themeLoader from '../languages/extensions/themeLoader'
 
 // Export 3 new colors used
 registerColor('statusBar.foreground', {
@@ -23,45 +19,3 @@ registerColor('statusBar.border', {
   hcDark: '#41454a',
   hcLight: '#dadada'
 }, 'Status bar border color.')
-
-interface DefaultTheme extends IThemeExtensionPoint {
-  extension: string
-}
-
-interface VSCodeTheme extends IThemeExtensionPoint {
-  load: () => Promise<string>
-}
-
-setDefaultThemes((defaultThemes as DefaultTheme[]).map(themeExtensionPoint => ({
-  ...themeExtensionPoint,
-  load: async () => JSON.stringify(await themeLoader[`${themeExtensionPoint.extension}:${themeExtensionPoint.path.slice(1)}`]!())
-})), async (themeExtensionPoint) => themeExtensionPoint.load())
-
-let themes: VSCodeTheme[] = []
-function updateThemes () {
-  setThemes(themes, async (themeExtensionPoint) => themeExtensionPoint.load())
-}
-updateThemes()
-
-function defineVSCodeTheme (idOrTheme: string | Pick<IThemeExtensionPoint, 'id' | 'label' | 'description' | 'uiTheme' | 'path'>, load: () => Promise<string>): Disposable {
-  const theme: VSCodeTheme = {
-    ...(typeof idOrTheme === 'string' ? { id: idOrTheme, path: `/${idOrTheme}.json` } : idOrTheme),
-    load,
-    _watch: false
-  }
-  themes = [
-    ...themes,
-    theme
-  ]
-  updateThemes()
-  return {
-    dispose () {
-      themes = themes.filter(_theme => _theme !== theme)
-      updateThemes()
-    }
-  }
-}
-
-export {
-  defineVSCodeTheme
-}
