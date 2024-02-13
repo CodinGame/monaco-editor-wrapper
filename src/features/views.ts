ab58bc0ba12789699119cb2584a63abda5c06ed0
Loïc Mangeonjean <loic@coderpad.io>
2024-02-13 19:33:55 +0100

    fix: dedup service overrides

--- src/features/views.ts
+++ src/features/views.ts
@@ -1,7 +1,5 @@
 import getViewsServiceOverride from '@codingame/monaco-vscode-views-service-override'
-import getOutputServiceOverride from '@codingame/monaco-vscode-output-service-override'
 import getStatusBarServiceOverride from '@codingame/monaco-vscode-view-status-bar-service-override'
-import getMarkersServiceOverride from '@codingame/monaco-vscode-markers-service-override'
 import getQuickAccessServiceOverride from '@codingame/monaco-vscode-quickaccess-service-override'
 import { editorOpenHandlerRegistry, registerServices, useGlobalPicker } from '../services'
 
@@ -15,9 +13,7 @@ registerServices({
   ...getViewsServiceOverride((model, input, sideBySide) => {
     return editorOpenHandlerRegistry.openCodeEditor(model, input, sideBySide)
   }),
-  ...getOutputServiceOverride(),
   ...getStatusBarServiceOverride(),
-  ...getMarkersServiceOverride(),
   ...getQuickAccessServiceOverride({
     shouldUseGlobalPicker () {
       return useGlobalPicker()
