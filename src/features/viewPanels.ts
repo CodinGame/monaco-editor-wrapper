650de438f1452ca3a144cc58240003c836dde924
Loïc Mangeonjean <loic@coderpad.io>
2024-02-25 16:51:46 +0100

    fix: missing worker type

--- src/features/viewPanels.ts
+++ src/features/viewPanels.ts
@@ -7,7 +7,7 @@ import { registerWorkerLoader } from '../worker'
 import { Worker } from '../tools/crossOriginWorker'
 import '@codingame/monaco-vscode-references-view-default-extension'
 
-registerWorkerLoader('outputLinkComputer', () => new Worker(new URL('@codingame/monaco-vscode-output-service-override/worker', import.meta.url)))
+registerWorkerLoader('outputLinkComputer', () => new Worker(new URL('@codingame/monaco-vscode-output-service-override/worker', import.meta.url), { type: 'module' }))
 
 registerServices({
   ...getTimelineServiceOverride(),
