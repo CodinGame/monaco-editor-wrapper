1369f59c2a3e302fd30220799c3ad11d6974167c
Loïc Mangeonjean <loic@coderpad.io>
2024-02-26 18:39:07 +0100

    feat: workbench feature

---
+++ src/features/workbench.ts
@@ -0,0 +1,21 @@
+import getWorkbenchServiceOverride from '@codingame/monaco-vscode-workbench-service-override'
+import getQuickAccessServiceOverride from '@codingame/monaco-vscode-quickaccess-service-override'
+import { registerServices, useGlobalPicker } from '../services'
+
+import '@codingame/monaco-vscode-theme-seti-default-extension'
+import '@codingame/monaco-vscode-media-preview-default-extension'
+import '@codingame/monaco-vscode-markdown-language-features-default-extension'
+import '@codingame/monaco-vscode-markdown-math-default-extension'
+import '@codingame/monaco-vscode-configuration-editing-default-extension'
+
+registerServices({
+  ...getWorkbenchServiceOverride(),
+  ...getQuickAccessServiceOverride({
+    shouldUseGlobalPicker () {
+      return useGlobalPicker()
+    },
+    isKeybindingConfigurationVisible () {
+      return true
+    }
+  })
+})
