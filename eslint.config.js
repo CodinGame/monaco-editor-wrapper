57559e9080ee6ec89b6417864938f429a9d549f7
Loïc Mangeonjean <loic@coderpad.io>
2025-02-04 11:59:16 +0100

    lib!: update all dependencies

---
+++ eslint.config.js
@@ -0,0 +1,6 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+  rules: {"@typescript-eslint/no-unused-vars": ["error", { "ignoreRestSiblings": true }]}
+})
